58b1841baa3f52a739559c516eb1f38f7e60eb17
Muhammad Sufian Javed <sufian_jawed@hotmail.com>
2020-06-09 13:48:52 +0500

    Update DevicePage.java
    added List of devices

--- src/test/java/pages/inventa/discover/DevicePage.java
+++ src/test/java/pages/inventa/discover/DevicePage.java
@@ -1,16 +1,18 @@
 package pages.inventa.discover;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import utils.selenium.SeleniumFactory;
+
+import java.util.List;
 
 public class DevicePage  extends SeleniumFactory {
 
     @FindBy(xpath="*//div[contains(text(), 'Devices')]") public static WebElement device_caption;
     @FindBy(xpath="//button[contains(text(), 'Query Wizard')]") private WebElement queryWizard_btn;
     @FindBy(xpath="//td[contains(text(), 'DEVICE')]") private WebElement randomDevice_gridResult;
-	@FindBy(xpath = "//td[@class='ng-tns-c14-10 ng-star-inserted']") public List<WebElement> List_randomDevice_gridResult;
     @FindBy(xpath="//input[@class='search-field']") private WebElement search_field;
 
 
@@ -18,12 +20,14 @@ public class DevicePage  extends SeleniumFactory {
     public void selectDevice()
     {
         waitForElement(device_caption);
-		for (int i = 0; i < List_randomDevice_gridResult.length; i++) {
-	    	if(List_randomDevice_gridResult.get(i)).contains("DEVICE")
-			{
-				System.out.println("Working fine");
-			}
-	    }
+
+        List<WebElement> List_randomDevice_gridResult = driver.findElements(By.xpath("//td[@class='ng-tns-c14-10 ng-star-inserted']"));
+        for(WebElement deviceName:List_randomDevice_gridResult){
+            if(deviceName.getText().contains("DEVICE"))
+            {
+                System.out.println("Working fine");
+            }
+        }
         click(randomDevice_gridResult);
     }
 
